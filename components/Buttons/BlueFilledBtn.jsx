a8f78223b7e8f9fd215009249a5c497c49b10d70
programmer9176 <myeducation9176@gmail.com>
2025-09-19 12:39:18 +0500

    updated btn

--- components/Buttons/BlueFilledBtn.jsx
+++ components/Buttons/BlueFilledBtn.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const BlueFilledBtn = ({title, url}) => {
   return (
-    <Link href={url} className='px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition focus:outline-none w-max inline-block'>
+    <Link href={url} className='px-6 py-2 md:py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition focus:outline-none w-max inline-block'>
       {title}
     </Link>
   )
--- components/Buttons/BlueOutlineBtn.jsx
+++ components/Buttons/BlueOutlineBtn.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const BlueOutlineBtn = ({title, url}) => {
   return (
     <>
-        <Link href={url} className='px-6 py-3 border border-blue-600 text-blue-600 rounded hover:bg-blue-600 hover:text-white transition focus:outline-none w-max inline-block'>
+        <Link href={url} className='px-6 py-2 md:py-3 border border-blue-600 text-blue-600 rounded hover:bg-blue-600 hover:text-white transition focus:outline-none w-max inline-block'>
             {title}
         </Link>
     </>
